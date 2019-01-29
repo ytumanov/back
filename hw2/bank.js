e78de96f7c37594821eb5358f4147a5a73170ba8
Yaroslav Tumanov <airhole.ua@gmail.com>
2019-01-30 01:44:01 +0200

    hw 2

---
+++ hw2/bank.js
@@ -0,0 +1,115 @@
+
+'use strict';
+
+const EventEmitter = require('events');
+
+class Bank extends EventEmitter{
+  constructor() {
+    super();
+    this.persons = [];
+    this.lastId = 1;
+  }
+
+  register(person) {
+    this._validateNewPerson(person);
+    
+    this.persons.push({
+      name: person.name,
+      balance: person.balance,
+      limit: person.limit
+    });
+
+    return this.lastId++;
+  }
+
+  add(id, balance) {
+    this._isValidId(id);
+    if (balance <= 0) {
+      this.emit('error', `Not valid balance to add provided`);
+    }
+    const person = this._getPersonById(id);
+    person.balance += balance;
+  }
+
+  withdraw(id, balanceToRemove) {
+    this._isValidId(id);
+
+    const person = this._getPersonById(id);
+
+    if (balanceToRemove <= 0) {
+      this.emit('error', `Not valid balance to remove provided`);
+    }
+
+    this._isLimitValid(person, balanceToRemove);
+
+    if (balanceToRemove > person.balance) {
+      this.emit('error', `Balance to remove is more than user has`);
+    }
+    person.balance -= balanceToRemove;
+  }
+
+  send(idFrom, idTo, value) {
+    this._isValidId(idFrom);
+    this._isValidId(idTo);
+
+    const from = this._getPersonById(idFrom);
+    const to = this._getPersonById(idTo);
+
+    if (value <= 0) {
+      this.emit('error', `Not valid value to send provided`);
+    }
+
+    if (value > from.balance) {
+      this.emit('error', `Sender could not send more than his balance`);
+    }
+
+    this._isLimitValid(from, value);
+    
+    from.balance -= value;
+    to.balance += value;
+  }
+
+  get(id, func) {
+    this._isValidId(id);
+    const person = this._getPersonById(id);
+    func(person.balance, person.name);
+  }
+
+  changeLimit(id, func) {
+    this._isValidId(id);
+
+    const person = this._getPersonById(id);
+    person.limit = func;
+  }
+
+  _isLimitValid(person, amount) {
+    if (!person.limit(amount, person.balance, person.balance - amount)) {
+      this.emit('error', `Not allowed limit provided`);
+    }
+  }
+
+  _getPersonById(id) {
+    return this.persons[id - 1];
+  }
+
+  _isValidId(id) {
+    if(this._getPersonById(id) === undefined) {
+      this.emit('error', `Person with provided id is not exist`);
+    }
+  }
+
+  _validateNewPerson(person) {
+    this.persons.forEach(p => {
+      if (p.name === person.name) {
+        this.emit('error', `Name ${person.name} is in use`);
+        //throw new Error(`Name ${personName} is in use`);
+      }
+    });
+
+    if (person.balance <= 0) {
+      this.emit('error', `Not valid start balance provided`);
+    }
+  }
+}
+
+module.exports = Bank;
---
+++ hw2/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Bank = require('./bank');
+
+const bank = new Bank();
+
+bank.on('add', (id, addBalance) => {
+  bank.add(id, addBalance);
+});
+
+bank.on('withdraw', (id, balanceToRemove) => {
+  bank.withdraw(id, balanceToRemove);
+});
+
+bank.on('changeLimit', (id, func) => {
+  bank.changeLimit(id, func);
+});
+
+bank.on('get', (id, func) => {
+  bank.get(id, func);
+})
+
+bank.on('error', error => {
+  throw error;
+});
+
+bank.on('send', (idFrom, idTo, value) => {
+  bank.send(idFrom, idTo, value);
+})
+
+
+const personId = bank.register({
+  name: 'Oliver White',
+  balance: 700,
+  limit: (amount, currentBalance, updatedBalance) => amount < 10
+});
+
+const personIdBlack = bank.register({
+  name: 'Oliver Black',
+  balance: 700,
+  limit: (amount, currentBalance, updatedBalance) => amount < 10
+});
+
+bank.emit('withdraw', personId, 5);
+bank.emit('get', personId, (amount, name) => {
+  console.log(`${name} has ${amount}₴`); // I have 695₴
+});
+// Вариант 1
+bank.emit('changeLimit', personId, (amount, currentBalance, updatedBalance) => {
+  return amount < 100 && updatedBalance > 650;
+});
+bank.emit('withdraw', personId, 15);
+bank.emit('get', personId, (amount, name) => {
+  console.log(`${name} has ${amount}₴`); // I have 680₴
+});
+
+//test limit on send
+bank.send(personId, personIdBlack, 20);
+
+bank.emit('get', personId, (amount, name) => {
+  console.log(`${name} has ${amount}₴`); // I have 660₴
+});
+
+bank.emit('get', personIdBlack, (amount, name) => {
+  console.log(`${name} has ${amount}₴`); // I have 720₴
+});
+
+//test limit on send
+bank.send(personId, personIdBlack, 20); // Error , updated balance limit is 650 and it's > new balance 640
